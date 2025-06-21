4bfaf87b357527eb33a3849ed1aa1245fafeb1ff
c0pson <pc312199@student.polsl.pl>
2025-06-21 19:57:54 +0200

    Player

---
+++ src/player.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from board import Board
+    from board import Node
+
+class Player:
+    def __init__(self, board: 'Board', name: str) -> None:
+        self.board: 'Board' = board
+        self.name: str = name
+        self.money: int = 10_000
+        self.current_position: 'Node' | None = board.board.head
+    
+    def move(self, steps: int) -> None:
+        self.board.move_player(self, steps)
+    
+    def move_to(self, position: int) -> None:
+        self.board.move_player_to_position(self, position)
